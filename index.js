74b6197c9b7d9545a1d6094c84feda32b8ddeb16
olgakahraman <ol.khrmn@gmail.com>
2023-09-17 23:39:52 -0400

    object destructuring + .map() + .join()

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-import { menuArray } from './data.js';
-console.log(menuArray);
+import { menuArray } from "./data.js";
+
+
+const menuHtml = menuArray.map((item) => {
+    const { name, ingredients, id, price, emoji } = item;
+
+    return `
+    <section class = "card">
+    <div class = "card-start">${emoji}</div>
+    <div class="card-middle">
+    <h2>${name}</h2>
+    <p>${ingredients}</p>
+    <h3>$${price}</h3></div>
+    <div class="card-end"><button id="${id}">+</button></div>
+    
+    </div>
+    </section>
+<hr />
+    `;
+  }).join(' ');
+
+document.getElementById('main').innerHTML = menuHtml;
